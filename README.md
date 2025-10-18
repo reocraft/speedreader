a6bda0d637e0779ca8e1fe286257e5cf7a55cb12
reocraft <reocraft.creeper@gmail.com>
2025-10-18 00:12:03 -0500

    Made improvements and added javadoc comments. Also fixed command line arguments

--- README.md
+++ README.md
@@ -4,4 +4,8 @@ This generates text on a screen which we imported from a .txt file.
 Oracle API documentations: Used to check the scanner class.
 Rayane Nossair: Asked some questions regarding the setup of pom.xml files.
 Java 24.0.2
-Visual Studio Code 1.104.0
+Visual Studio Code 1.105.1
+
+When running the program:
+Format:
+mvn exec:java -Dexec.args="filename width height font_size wpm"
--- src/main/java/edu/grinnell/csc207/speedreader/SpeedReader.java
+++ src/main/java/edu/grinnell/csc207/speedreader/SpeedReader.java
@@ -20,10 +20,18 @@ public class SpeedReader {
             txt = new Scanner(new File(filename));
         }
 
+        /**
+         * Checks if the word generator has a next string.
+         * @return true if there is a next string, false otherwise.
+         */
         public boolean hasNext() {
             return txt.hasNext();
         }
 
+        /**
+         * Returns the next string in the word generator.
+         * @return the next word as a string.
+         */
         public String next() {
             if (txt.hasNext()) {
                 wordCount++;
@@ -36,11 +44,24 @@ public class SpeedReader {
             return word;
         }
 
+        /**
+         * Returns `sentenceCount` which counts how many punctuations there are in the word generator.
+         * @return the number of punctuations counted which is stored in `sentenceCount`.
+         */
         public int getSentenceCount() {
             return sentenceCount;
         }
     }
 
+    /**
+     * Makes a screen that takes inputs of size and wpm which is passed through command line arguments.
+     * @param w word generator that contains the words that will be displayed.
+     * @param width horizontal dimensions of the screen.
+     * @param height vertical dimensions of the screen.
+     * @param fontSize the size of the font of the words displayed on the screen.
+     * @param wpm the speed that the words will be shown on the screen.
+     * @throws InterruptedException
+     */
     static void animateWords(WordGenerator w, int width, int height, int fontSize, int wpm) throws InterruptedException {
         DrawingPanel panel = new DrawingPanel(width, height);
         Graphics g = panel.getGraphics();
@@ -50,12 +71,13 @@ public class SpeedReader {
         Color black = new Color(1);
         
         while(w.hasNext()) {
+            String cur = w.next();
             g.setColor(white);
             g.fillRect(0, 0, width, height);
             g.setColor(black);
-            g.drawString(w.next(), width/2, height/2);
-            // math missing here for wpm to sleep conversion
-            Thread.sleep(1000);
+            g.drawString(cur, width/2 - fontSize * cur.length()/2, height/2);
+            // width/2 - fontSize * cur.length()/2 ensures that words will be centered even for longer words.
+            Thread.sleep(1000/wpm*60);
         }
     }
 
